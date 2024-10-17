55235ddd5a3a119a256cd8fcacd06e65a179c381
fereline0 <shimko.alex.fereline@gmail.com>
2024-10-17 17:16:28 +0300

    Добавьте файлы проекта.

---
+++ any/Data/anyContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using any.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace any.Data
+{
+    public class anyContext : DbContext
+    {
+        public anyContext(DbContextOptions<anyContext> options)
+            : base(options) { }
+
+        public DbSet<any.Models.User> User { get; set; } = default!;
+
+        public override int SaveChanges()
+        {
+            AddTimestamps();
+            return base.SaveChanges();
+        }
+
+        public override async Task<int> SaveChangesAsync(
+            CancellationToken cancellationToken = new CancellationToken()
+        )
+        {
+            AddTimestamps();
+            return await base.SaveChangesAsync();
+        }
+
+        private void AddTimestamps()
+        {
+            var entities = ChangeTracker
+                .Entries()
+                .Where(x =>
+                    x.Entity is BaseEntity
+                    && (x.State == EntityState.Added || x.State == EntityState.Modified)
+                );
+
+            foreach (var entity in entities)
+            {
+                var now = DateTime.UtcNow;
+
+                if (entity.State == EntityState.Added)
+                {
+                    ((BaseEntity)entity.Entity).CreatedAt = now;
+                }
+                ((BaseEntity)entity.Entity).UpdatedAt = now;
+            }
+        }
+    }
+}
---
+++ any/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace any.Models
+{
+    public class User : BaseEntity
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
